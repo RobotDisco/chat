fd0fcb501db44b212dbdc9fba02d0bb77700e030
Gaelan D'costa <gdcosta@gmail.com>
2018-08-31 00:19:20 -0400

    Bind to port 10000, still do nothing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate mio;
+use mio::tcp::*;
 use mio::*;
+use std::net::SocketAddr;
 
 struct WebSocketServer;
 
@@ -14,10 +16,19 @@ impl Handler for WebSocketServer {
 }
 
 fn main() {
+    // Open and bind a TCP port:
+    let address = "0.0.0.0:10000".parse::<SocketAddr>().unwrap();
+    let server_socket = TcpListener::bind(&address).unwrap();
+
+    // Register socket with event loop:
     let mut event_loop = EventLoop::new().unwrap();
+    event_loop.register(&server_socket,
+                        Token(0),
+                        EventSet::readable(),
+                        PollOpt::edge()).unwrap();
+
     // Create a new instance of our handler struct:
     let mut handler = WebSocketServer;
     // ... and then provide the event loop with a mutable reference to it:
     event_loop.run(&mut handler).unwrap();
-
 }
