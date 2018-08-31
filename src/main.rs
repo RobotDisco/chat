6055f0025abc0370e4914a4cbe78e0cdbd559030
Gaelan D'costa <gdcosta@gmail.com>
2018-08-31 00:59:57 -0400

    Accept and register initiated connections from listener port.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@ extern crate mio;
 use mio::tcp::*;
 use mio::*;
 use std::net::SocketAddr;
+use std::collections::HashMap;
 
-struct WebSocketServer;
+struct WebSocketServer {
+    socket: TcpListener,
+    clients: HashMap<Token, TcpStream>,
+    token_counter: usize
+}
+
+const SERVER_TOKEN: Token = Token(0);
 
 impl Handler for WebSocketServer {
     // Traits can have useful default implementations, so in fact the handler
@@ -13,6 +20,32 @@ impl Handler for WebSocketServer {
     // anytime soon, so let's get along with the defaults from the mio examples:
     type Timeout = usize;
     type Message = ();
+
+    fn ready(&mut self, event_loop: &mut EventLoop<WebSocketServer>,
+             token: Token, _events: EventSet)
+    {
+        match token {
+            SERVER_TOKEN => {
+                let client_socket = match self.socket.accept() {
+                    Err(e) => {
+                        println!("Accept error: {}", e);
+                        return;
+                    },
+                    Ok(None) => unreachable!("Accept has returned 'None"),
+                    Ok(Some((sock, _addr))) => sock
+                };
+
+                self.token_counter += 1;
+                let new_token = Token(self.token_counter);
+
+                self.clients.insert(new_token, client_socket);
+                event_loop.register(&self.clients[&new_token],
+                                    new_token, EventSet::readable(),
+                                    PollOpt::edge() | PollOpt::oneshot()).unwrap();
+            },
+            _token => ()
+        }
+    }
 }
 
 fn main() {
@@ -23,12 +56,17 @@ fn main() {
     // Register socket with event loop:
     let mut event_loop = EventLoop::new().unwrap();
     event_loop.register(&server_socket,
-                        Token(0),
+                        SERVER_TOKEN,
                         EventSet::readable(),
                         PollOpt::edge()).unwrap();
 
     // Create a new instance of our handler struct:
-    let mut handler = WebSocketServer;
+    let mut server = WebSocketServer {
+        token_counter: 1, // Starting the token counter from 1
+        clients: HashMap::new(), // Creating an empty HashMap
+        socket: server_socket // Handling the ownership of the socket to the struct
+    };
+
     // ... and then provide the event loop with a mutable reference to it:
-    event_loop.run(&mut handler).unwrap();
+    event_loop.run(&mut server).unwrap();
 }
