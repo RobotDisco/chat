28acaa95733545927bd8369f38627b95a6b1dc8c
Gaelan D'costa <gdcosta@gmail.com>
2018-08-30 23:41:21 -0400

    do-nothing networking handler event loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate mio;
+use mio::*;
+
+struct WebSocketServer;
+
+impl Handler for WebSocketServer {
+    // Traits can have useful default implementations, so in fact the handler
+    // interface requires us to provide only two things: concrete types for
+    // timeouts and messages.
+    // We're not ready to cover these fancy details, and we wouldn't get to them
+    // anytime soon, so let's get along with the defaults from the mio examples:
+    type Timeout = usize;
+    type Message = ();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut event_loop = EventLoop::new().unwrap();
+    // Create a new instance of our handler struct:
+    let mut handler = WebSocketServer;
+    // ... and then provide the event loop with a mutable reference to it:
+    event_loop.run(&mut handler).unwrap();
+
 }
